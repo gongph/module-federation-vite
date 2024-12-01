bebdac33388f94ce16389a31c4632de2d810185a
gongph <gongph@foxmail.com>
2024-12-01 08:42:37 +0800

    feat: add MF

--- host-vue/vite.config.js
+++ host-vue/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { federation } from "@module-federation/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    federation({
+      name: "host",
+      remotes: {
+        remote: "http://localhost:5176/testbase/mf-manifest.json",
+      },
+      shared: {
+        vue: {},
+      },
+      filename: "remoteEntry-[hash].js",
+      manifest: true,
+    }),
+  ],
+  build: {
+    target: "chrome89",
+  },
+});
--- remote-vue/vite.config.js
+++ remote-vue/vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { federation } from "@module-federation/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  preview: {
+    port: 5176,
+  },
+  server: {
+    open: true,
+    port: 5176,
+    origin: "http://localhost:5176",
+  },
+  base: "http://localhost:5176/testbase",
+  plugins: [
+    vue(),
+    federation({
+      name: "remote-vue",
+      filename: "remoteEntry-[hash].js",
+      exposes: {
+        ".": "./src/App.vue",
+      },
+      manifest: true,
+      shared: {
+        vue: {},
+      },
+    }),
+  ],
+  build: {
+    target: "chrome89",
+    rollupOptions: {
+      output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+      },
+    },
+  },
+});
